40dd1095dabc1c2adda6d899b865e8d3be2eec77
SamfortWolf <54212546+SamfortWolf@users.noreply.github.com>
2019-10-20 20:00:20 +0500

    HW03 main part

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE meal_seq START WITH 100000;
 
 CREATE TABLE users
 (
@@ -23,3 +24,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id                  INTEGER PRIMARY KEY DEFAULT nextval('meal_seq'),
+    userId              INTEGER             NOT NULL,
+    datetime            TIMESTAMP           NOT NULL,
+    description         VARCHAR             NOT NULL,
+    calories            INTEGER             NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_datetime_idx ON meals (datetime, userId);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,7 @@
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE meal_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +10,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT into meals (userId, description, datetime, calories) VALUES
+    (100000, 'Завтрак',  '2015-05-30 10:00:00', 500),
+    (100000, 'Обед',     '2015-05-30 13:00:00', 1000),
+    (100000, 'Ужин',     '2015-05-30 20:00:00', 500),
+    (100000, 'Завтрак',  '2015-05-31 10:00:00', 1000),
+    (100000, 'Обед',     '2015-05-31 13:00:00', 500),
+    (100000, 'Ужин',     '2015-05-31 20:00:00', 510);
