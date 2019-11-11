ff0e27d10842054341581f3f72f6da30f1f77d2c
SamfortWolf <54212546+SamfortWolf@users.noreply.github.com>
2019-11-11 15:10:07 +0300

    6_11_spring_web.patch

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -24,13 +24,7 @@ import java.util.Set;
 
 import static ru.javawebinar.topjava.UserTestData.*;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml",
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+
 public class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Autowired
